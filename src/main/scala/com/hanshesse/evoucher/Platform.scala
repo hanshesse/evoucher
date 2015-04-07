55437322c94087f318f42f24e9fb7a305b3e367e
Hans Hesse <hans.hesse@gmail.com>
2015-04-07 17:50:06 +0200

    Added a basic voucher model

--- src/main/scala/com/hanshesse/evoucher/Platform.scala
+++ src/main/scala/com/hanshesse/evoucher/Platform.scala
@@ -4,21 +4,9 @@
 
 package com.hanshesse.evoucher
 
-import akka.actor.{ActorSystem, Props}
-import akka.io.IO
-import spray.can.Http
-import akka.pattern.ask
-import akka.util.Timeout
-import scala.concurrent.duration._
 
 object Platform extends App {
-  // we need an ActorSystem to host our application in
-  implicit val system = ActorSystem("on-spray-can")
+  val v = Voucher("hans", "hesse")
 
-  // create and start our service actor
-  val service = system.actorOf(Props[MyServiceActor], "demo-service")
-
-  implicit val timeout = Timeout(5.seconds)
-  // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8081)
+  print(v)
 }
---
+++ src/main/scala/com/hanshesse/evoucher/Voucher.scala
@@ -0,0 +1,6 @@
+package com.hanshesse.evoucher
+
+/**
+ * Created by Hans Hesse on 2015/04/07.
+ */
+case class Voucher(serial: String, PIN: String)
